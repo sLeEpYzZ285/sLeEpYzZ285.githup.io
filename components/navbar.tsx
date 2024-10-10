a2d257e6cd24c68527e7ddc50d94dab184c09452
PROxTae <nithis285@gmail.com>
2024-10-11 02:46:30 +0700

    nav menu

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,3 +1,4 @@
+'use client'
 import {
   Navbar as NextUINavbar,
   NavbarContent,
@@ -14,7 +15,7 @@ import { Input } from "@nextui-org/input";
 import { link as linkStyles } from "@nextui-org/theme";
 import NextLink from "next/link";
 import clsx from "clsx";
-
+import { useState, useEffect, useMemo } from "react";
 import { siteConfig } from "@/config/site";
 import { ThemeSwitch } from "@/components/theme-switch";
 import {
@@ -28,6 +29,8 @@ import {
 import Image from "next/image";
 
 export const Navbar = () => {
+  const [navOpen, setNavOpen] = useState(false)
+
   const searchInput = (
     <Input
       aria-label="Search"
@@ -50,7 +53,7 @@ export const Navbar = () => {
   );
 
   return (
-    <NextUINavbar maxWidth="xl" position="sticky" shouldHideOnScroll>
+    <NextUINavbar  isMenuOpen={navOpen} maxWidth="xl" position="sticky" shouldHideOnScroll>
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
         <NavbarBrand as="li" className="gap-3 max-w-fit">
           <NextLink className="flex justify-start items-center gap-1" href="/">
@@ -121,7 +124,7 @@ export const Navbar = () => {
           <GithubIcon className="text-default-500" />
         </Link> */}
         <ThemeSwitch />
-        <NavbarMenuToggle />
+        <NavbarMenuToggle onChange={() => setNavOpen(!navOpen)} />
       </NavbarContent>
 
       <NavbarMenu>
@@ -129,19 +132,19 @@ export const Navbar = () => {
         <div className="mx-4 mt-2 flex flex-col gap-2">
           {siteConfig.navMenuItems.map((item, index) => (
             <NavbarMenuItem key={`${item}-${index}`}>
-              <Link
-                color={
-                  index === 2
-                    ? "primary"
-                    : index === siteConfig.navMenuItems.length - 1
-                      ? "danger"
-                      : "foreground"
-                }
-                href="#"
-                size="lg"
+              <NextLink
+
+                className={clsx(
+                  linkStyles({ color: "foreground" }),
+                  "data-[active=true]:text-primary data-[active=true]:font-medium",
+                )}
+                onClick={() => {  setNavOpen(!navOpen) }}
+                color="foreground"
+                href={item.href}
+                
               >
                 {item.label}
-              </Link>
+              </NextLink>
             </NavbarMenuItem>
           ))}
         </div>
--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
   // output: 'export',
   // ปรับ basePath ตาม repository ที่ใช้
-  basePath: '/sleepyzz285.github.io/sLeEpYzZ285.githup.io',
-  assetPrefix: '/sLeEpYzZ285.githup.io/',
+  // basePath: '/sleepyzz285.github.io',
+  // assetPrefix: '/sLeEpYzZ285.githup.io/',
   images: {
     domains: [], // ปล่อยให้เป็น array ว่างๆ เพื่ออนุญาตทุกโดเมน
     unoptimized: true, // ปิดการปรับแต่งรูปภาพเพื่อไม่จำกัดแหล่งที่มา
